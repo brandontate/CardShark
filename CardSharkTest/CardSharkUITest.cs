6123ec8bd424545580fe868f1d37304ba1e7e2a7
brandontate <blt900@gmail.com>
2015-02-05 19:10:33 -0600

    Added Test Files

--- CardSharkTest/CardSharkUITest.cs
+++ CardSharkTest/CardSharkUITest.cs
@@ -5,39 +5,37 @@ using TestStack.White;
 using TestStack.White.Factory;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.WindowItems;
-using TestStack.BDDfy;
 
 namespace CardSharkTest
 {
-    /// <summary>
-    /// Summary description for StoryTest
-    /// </summary>
+    
     [TestClass]
-    public class StoryTest
+    public class CardSharkUITest
     {
+        private static TestContext test_context;
         private static Window window;
         private static Application application;
 
         [ClassInitialize]
-        public static void Setup(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext _context)
+        public static void Setup(TestContext _context)
         {
+            test_context = _context;
             var applicationDir = _context.DeploymentDirectory;
             var applicationPath = Path.Combine(applicationDir, "..\\..\\..\\CardSharkTest\\bin\\Debug\\CardShark");
             application = Application.Launch(applicationPath);
             window = application.GetWindow("MainWindow", InitializeOption.NoCache);
         }
-
+        
         [TestMethod]
-        public void TestComboBoxItems()
+        public void TestZeroState()
         {
             WPFComboBox organizationcombo = window.Get<WPFComboBox>("OrganizationComboBox");
             WPFComboBox eventcombo = window.Get<WPFComboBox>("EventComboBox");
-
-            ListBox test = window.Get<ListBox>("Test");
-
-            Assert.AreEqual(organizationcombo.Items[0].Text, "WWE");
-            Assert.AreEqual(organizationcombo.Items[1].Text, "UFC");
+           
+            Assert.IsTrue(organizationcombo.Enabled);
+            Assert.IsFalse(eventcombo.Enabled);
         }
 
         [ClassCleanup]
@@ -45,6 +43,6 @@ namespace CardSharkTest
         {
             window.Close();
             application.Close();
-        }   
+        }
     }
 }
